280153a32ae6c0cf91d327671df0bc14787377c8
markjrieke <markjrieke@gmail.com>
2025-07-27 14:36:33 -0500

    update main.py w/command line args

--- main.py
+++ main.py
@@ -1,22 +1,48 @@
-import arviz as az
+from argparse import ArgumentParser
 
 from war.data import WARData
 from war.model import WARModel
+from war.results import WARResults
 
-war_data = WARData().prep_data()
+parser = ArgumentParser(
+    prog='Bayesian WAR',
+    description='Estimate candidate WAR via hierarchical Bayesian model'
+)
+
+# Command line arguments for sampling
+parser.add_argument('--chamber', choices=['house', 'senate'], required=True)
+parser.add_argument('--iter_warmup', type=int, default='1000')
+parser.add_argument('--iter_sampling', type=int, default='1000')
+parser.add_argument('--chains', type=int, default='10')
+parser.add_argument('--parallel_chains', type=int, default='10')
+
+args = parser.parse_args()
+
+# Import and prep data for running the model
+war_data = WARData(args.chamber).prep_data()
+
+# Fit the mode.
 war_fit = (
     WARModel(
-        data=war_data,
-        stan_file='stan/dev_01.stan',
+        war_data=war_data,
+        stan_file='stan/war.stan',
         dir='exe'
     )
+    .prep_stan_data()
     .sample(
-        iter_warmup=1000,
-        iter_sampling=1000,
-        chains=8,
-        parallel_chains=8
+        iter_warmup=args.iter_warmup,
+        iter_sampling=args.iter_sampling,
+        chains=args.chains,
+        parallel_chains=args.parallel_chains,
+        inits=0.01,
+        step_size=0.002,
+        refresh=20,
+        seed=2026
     )
 )
 
-# TODO blegh ----
-az.from_cmdstanpy(posterior=war_fit).posterior
+# Write results to out/
+war_results = WARResults(war_fit)
+war_results.write_full_topline()
+war_results.write_publication_topline()
+war_results.write_parameter_summaries()
