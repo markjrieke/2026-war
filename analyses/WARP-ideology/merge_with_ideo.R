7dd44288f8ff58ee95011d1167afdd57fa026f10
Elliott Morris <11248148+elliottmorris@users.noreply.github.com>
2025-08-12 17:04:21 -0400

    updating filter logic, adding a bootstrap

--- analyses/WARP-ideology/merge_with_ideo.R
+++ analyses/WARP-ideology/merge_with_ideo.R
@@ -18,10 +18,9 @@ pols = pols %>%
 # filter to winning candidates OR candidates who appear twice+
 pols = pols %>% 
   group_by(representative) %>%
-  filter(
-    (party == 'dem' & dem_pct > 0.5) | (party == 'rep' & dem_pct < 0.5) & 
-      (n() >= 2)
-  ) %>%
+  mutate(winner = 
+           (party == 'dem' & dem_pct > 0.5) | (party == 'rep' & dem_pct < 0.5) ) %>%
+  filter( winner |  (n() >= 0) ) %>%
   ungroup() # %>% group_by(representative) %>% filter(n() > 5)
 
 # join WAR
@@ -139,9 +138,9 @@ ggplot(dat[dat$cycle == 2024,], aes(x = abs(score), y = WAR*100)) +
   scale_color_manual(values=c('dem'='blue','rep'='red')) +
   geom_smooth(method = 'lm',col='gray40') +
   theme_minimal() +
-  labs(x = 'Ideological extermity (Bonica "composite score")\n(Lower is more moderate)',
+  labs(x = 'Ideological extremity (Bonica "composite score")\n(Lower is more moderate)',
        y = 'Wins Above Replacement (percentage points)',
-       subtitle = 'Politician WAR versus ideological extermity, 2024.\nThe most moderate members outperform the most ideologically extreme by ~3 points.',
+       subtitle = 'Politician WAR versus ideological extremity, 2024.\nThe most moderate members outperform the most ideologically extreme by ~3 points.',
        title = 'Moderates no longer have significantly higher WAR',
        col = '')
 
@@ -149,9 +148,9 @@ ggplot(dat, aes(x = abs(score), y = WAR*100)) +
   geom_point(shape = 1) +
   geom_smooth(method = 'lm',aes(col = as.character(cycle)),show.legend = T, se = F) +
   theme_minimal() +
-  labs(x = 'Ideological extermity (Bonica "composite score")\n(Lower is more moderate)',
+  labs(x = 'Ideological extremity (Bonica "composite score")\n(Lower is more moderate)',
        y = 'Wins Above Replacement (percentage points)',
-       subtitle = 'Politician WAR versus ideological extermity, 2000-2024',
+       subtitle = 'Politician WAR versus ideological extremity, 2000-2024',
        title = 'The electoral advantage of ideological moderation is declining',
        col = 'year')
 
@@ -161,9 +160,9 @@ ggplot(dat, aes(x = abs(score), y = WAR*100)) +
   geom_smooth(method = 'lm', aes (col = party), se=F,show.legend = F) +
   facet_wrap(~cycle) + 
   theme_minimal() +
-  labs(x = 'Ideological extermity (Bonica "composite score")\n(Lower is more moderate)',
+  labs(x = 'Ideological extremity (Bonica "composite score")\n(Lower is more moderate)',
        y = 'Wins Above Replacement (percentage points)',
-       subtitle = 'Politician WAR versus ideological extermity, 2000-2024',
+       subtitle = 'Politician WAR versus ideological extremity, 2000-2024',
        title = 'The electoral advantage of ideological moderation is declining')
 
 lm(gen_vote_pct ~ I(WAR*100),
@@ -179,8 +178,8 @@ lm(gen_vote_pct ~ I(WAR*100),
 ggplot(dat[(dat$uncontested == 0 & 
              dat$gen_vote_pct > 10 & 
              dat$gen_vote_pct < 90 &
-             dat$WAR*100 < 10 &
-             dat$WAR*100 > -10),], 
+             dat$WAR*100 < 20 &
+             dat$WAR*100 > -20),], 
        aes(x = WAR*100, y = gen_vote_pct-mean(gen_vote_pct,na.rm=T))) + 
   geom_point(shape = 1) + 
   geom_smooth(method = 'lm') +
@@ -192,6 +191,103 @@ ggplot(dat[(dat$uncontested == 0 &
        title = 'Candidates with higher WAR do better')
 
 
+lm(WAR ~ abs(score), 
+   data = dat %>% 
+     filter(cycle == 2024)) %>% summary
+
+# variance explained by WAR? ----------------------------------------------
+# trim data set
+temp = dat[(dat$uncontested == 0 & 
+               dat$dem_pct > .10 & 
+               dat$dem_pct < .90 &
+               dat$WAR*100 < 20 &
+               dat$WAR*100 > -20 &
+              dat$party == 'dem'),]
+        
 # add race-level vars
-races = read_csv('house_forecast_data_updated.csv')
+races = read_csv('house_forecast_data_updated.csv') %>% select(-uncontested)
 dat = dat %>% left_join(races, by = c('cycle','state_name','district'))
+
+
+# regression on partisanship
+lm(dem_pct ~ 1 + dem_pres_twop_lag_lean_one - cycle,
+   data = dat[dat$cycle == 2024 & dat$party == 'dem',] ) %>% summary
+
+
+lm(dem_pct ~ 1  + abs(score) + dem_pres_twop_lag_lean_one - cycle,
+   data = dat[dat$cycle == 2024 & dat$party == 'dem',] ) %>% summary
+
+
+lm(dem_pct ~ 1  + WAR + abs(score) + dem_pres_twop_lag_lean_one - cycle,
+   data = dat[dat$cycle == 2024 & dat$party == 'dem',] ) %>% summary
+
+
+# redo elects, make everyone more moderate -----------------
+library(mgcv)
+cycle_sims = map_df(seq(2012,2024,2),
+       function(yr){
+         print(yr)
+         # get data
+         tmp = dat %>% filter(cycle == yr)
+         
+         # get dems
+         dems = tmp[tmp$party == 'dem',]
+         
+         # fill in gaps
+         ideo_model = lm(score ~ dem_pres_twop_lag_lean, data = dems)
+         dems = dems %>%
+           mutate(score = ifelse(is.na(score), predict(ideo_model, newdata = .), score))
+       
+         # bootstrap
+         sims = map_df(1:5000,
+                function(i){
+                  # resample
+                  dems = sample_n(tbl = dems, size = nrow(dems), replace = T)
+                  
+                  # get relationship between covars and vote pct
+                  vote_model = lm(dem_pct ~ 1 +
+                                    WAR + 
+                                    dem_pres_twop_lag_lean + 
+                                    incumbent +
+                                    experience +
+                                    has_fec:dem_share_fec
+                                    , data = dems)
+                  
+                  # get relationship between ideo and WAR, for winners
+                  war_model = gam(WAR ~ s(score), data = dems)
+                  
+                  # now for each candidate, predict WAR based on actual values, and hypo values
+                  dems$WAR_hat = predict(war_model, newdata = dems)
+                  dems$hypo_WAR_hat = predict(war_model, 
+                                              newdata = (dems %>% mutate(score = score + 1)))
+                  
+                  dems$delta = dems$hypo_WAR_hat - dems$WAR_hat 
+                  
+                  # add the delta to WAR
+                  dems$WAR = dems$WAR + dems$delta
+                  
+                  # re-predict dem_pct
+                  dems$dem_pct_hat = predict(vote_model, newdata = dems)
+                  
+                  # sum up seats
+                  tibble(cycle = yr, iter = i, 
+                         dem_chg = sum(dems$dem_pct_hat > 0.5) - 
+                           sum(dems$dem_pct > 0.5)) %>%
+                    
+                    return
+                })
+         
+         return(sims)
+         
+       })
+
+# summarise
+sims = sims %>%
+  group_by(cycle) %>%
+  summarise(median = median(dem_chg),
+            upper = quantile(dem_chg,0.975),
+            lower = quantile(dem_chg,0.025)
+  )
+sims
+print(sims)
+
